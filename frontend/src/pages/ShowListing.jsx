b411b4d56241c9bdbe2e390b47b7c9daf770e76a
Shukardeen <sonugahlot57016@gmail.com>
2025-05-26 11:17:47 +0530

    Update ShowListing.jsx

--- frontend/src/pages/ShowListing.jsx
+++ frontend/src/pages/ShowListing.jsx
@@ -45,7 +45,7 @@ function ShowListing() {
 
   if (!listing || Object.keys(listing).length === 0) {
     return (
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full flex justify-center items-center mt-5">
         <Box sx={{ display: 'flex' }}>
           <CircularProgress />
         </Box>
